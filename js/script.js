75cde932af71303bda8f3d5828f2730284e73ad9
m78hayata <m78hayata@gmail.com>
2024-03-25 02:45:46 +0900

    change nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // DOMが読み込まれた後に実行されるコード
+    // 現在のページのURLを取得
+    var currentPageUrl = window.location.href;
+    
+    // すべてのナビゲーションリンクを取得
+    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    
+    navLinks.forEach(function(link) {
+        // リンクが現在のページのURLに一致する場合、.activeクラスを追加
+        if (link.href === currentPageUrl) {
+            link.classList.add('active');
+        }
+    });
 });
+
