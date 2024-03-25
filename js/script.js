7c665d24749ee7ec0d692f47c72ec58117920da2
m78hayata <m78hayata@gmail.com>
2024-03-26 01:12:16 +0900

    delete slide show

--- js/script.js
+++ js/script.js
@@ -11,18 +11,4 @@ document.addEventListener('DOMContentLoaded', function() {
             link.classList.add('active');
         }
     });
-
-    const images = document.querySelectorAll('.slide-image');
-    let currentImageIndex = 0;
-    images[currentImageIndex].style.opacity = 1; // 最初の画像を表示
-
-    function rotateImage() {
-        const nextImageIndex = (currentImageIndex + 1) % images.length;
-        images[currentImageIndex].style.opacity = 0; // 現在の画像をフェードアウト
-        images[nextImageIndex].style.opacity = 1; // 次の画像をフェードイン
-        currentImageIndex = nextImageIndex;
-    }
-
-    // 3秒ごとに画像を切り替える
-    setInterval(rotateImage, 3000);
 });
