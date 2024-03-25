d55b771058a83883e92c135172b210abb3a0f1b5
m78hayata <m78hayata@gmail.com>
2024-03-26 01:09:27 +0900

    revise slide show

--- js/script.js
+++ js/script.js
@@ -11,14 +11,16 @@ document.addEventListener('DOMContentLoaded', function() {
             link.classList.add('active');
         }
     });
-    
+
     const images = document.querySelectorAll('.slide-image');
     let currentImageIndex = 0;
+    images[currentImageIndex].style.opacity = 1; // 最初の画像を表示
 
     function rotateImage() {
-        images[currentImageIndex].style.display = 'none';
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        images[currentImageIndex].style.display = 'block';
+        const nextImageIndex = (currentImageIndex + 1) % images.length;
+        images[currentImageIndex].style.opacity = 0; // 現在の画像をフェードアウト
+        images[nextImageIndex].style.opacity = 1; // 次の画像をフェードイン
+        currentImageIndex = nextImageIndex;
     }
 
     // 3秒ごとに画像を切り替える
